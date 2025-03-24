d2bd71708e04469745fa71ed1d879261a0840278
paweln1975 <pawel.niedziela@gmail.com>
2025-03-24 21:25:19 +0100

    partial, partialmethod

--- 03-advanced/10-fp-paradigm/10.08-currying/notes.py
+++ 03-advanced/10-fp-paradigm/10.08-currying/notes.py
@@ -7,11 +7,37 @@
 # - functools.partialmethod()
 # %%
 
+from functools import partialmethod, partial
 
+round2 = partial(round, ndigits=2)
+numbers = [1.123, 2.3445, 3.984]
+rounded_numbers = list(map(round2, numbers))
+print(*rounded_numbers)
 
 # %% Problem
 # %%
 
+class Cell:
+    def __init__(self, alive: bool=True):
+        self._alive = alive
+
+    @property
+    def alive(self):
+        return self._alive
+
+    def set_state(self, state):
+        self._alive = bool(state)
+
+    set_alive = partialmethod(set_state, state=True)
+    set_dead = partialmethod(set_state, state=False)
+
+
+c = Cell()
+c.set_alive()
+print(c.alive)
+
+c.set_dead()
+print(c.alive)
 
 
 # %% Solution
