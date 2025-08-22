363fabcb9c943f893c30d1ddbe4f03b9955d29b8
wz6l1y <pawel.niedziela@aptiv.com>
2025-08-22 12:28:12 +0200

    checkers update

--- 22-challange/22-06-coverity-levels/process_checkers.py
+++ 22-challange/22-06-coverity-levels/process_checkers.py
@@ -15,11 +15,9 @@ with open(output_file, 'w', newline='', encoding='utf-8') as csvfile:
         checker_type = checker.get('type', '')
         impact = checker.get('impact', '')
         language = checker.get('language', '')
+        rule_strength = checker.get('ruleStrength', '')
         if not language:
             families = checker.get('families', [])
             if isinstance(families, list):
                 language = ','.join(families)
-        rule_strength = ''
-        if isinstance(checker_type, str) and checker_type.startswith('MISRA'):
-            rule_strength = checker.get('ruleStrength', '')
         writer.writerow([name, checker_type, impact, language, rule_strength])
